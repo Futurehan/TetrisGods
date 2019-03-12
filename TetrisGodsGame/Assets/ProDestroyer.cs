2ce8ad6f9c3e3278de09eccacd030f8189d325fe
Jonas <Kje.petersson@gmail.com>
2019-03-12 17:37:06 +0100

    Added another cannon that as of now does the same thing as the old one but it's going to be the destroy weapon when i'm done.

---
+++ TetrisGodsGame/Assets/ProDestroyer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProDestroyer : MonoBehaviour
+{
+    Rigidbody body;
+    BoxCollider boxy;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        boxy = GetComponent<BoxCollider>();
+    }
+
+
+    public void Fire(Vector3 forceDirection)
+    {
+        Vector3 shootForce = forceDirection.normalized * 30f;
+        body.AddForce(shootForce, ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other);
+        boxy.enabled = false;
+    }
+}
