2c3aa96d04d8475fb5848be1cd45ab7bb5e82db1
karl <karlfyrkant@gmail.com>
2019-03-15 14:54:06 +0100

    Added new graphics and fixed thrust animation

--- TetrisGodsGame/Assets/Scripts/Blocks/BlockController.cs
+++ TetrisGodsGame/Assets/Scripts/Blocks/BlockController.cs
@@ -18,6 +18,15 @@ public class BlockController : MonoBehaviour
     private BlockSpawner blockSpawner;
     private GameObject box;
     private GameManager.PlayerIndex owner;
+    private float thrustLerpDistance = 0;
+    private float thrustLerpSpeed = 15;
+    private float leftthrustLerpDistance = 0;
+    private float rightthrustLerpDistance = 0;
+
+
+    public GameObject downThrust;
+    public GameObject leftThrust;
+    public GameObject rightThrust;
 
     //Delegates
     public delegate void OnDestruction(BlockController boxObject, GameManager.PlayerIndex player);
@@ -47,6 +56,9 @@ public class BlockController : MonoBehaviour
     {
         if (activateSpawn)
         {
+            downThrust.SetActive(false);
+            leftThrust.SetActive(false);
+            rightThrust.SetActive(false);
             blockSpawner.CallNext();
             Active = false;
             activateSpawn = false;
@@ -59,6 +71,62 @@ public class BlockController : MonoBehaviour
         Vector3 turnInput = Input.GetAxis(horizontalID)*body.transform.forward * -100 *Time.deltaTime;
         body.AddTorque(turnInput);
         body.AddForce(forceInput);
+
+
+        //Thrust graphic handler
+        #region
+        if (Input.GetAxis(verticalID) != 0)
+        {
+            if (downThrust.transform.localScale.y < 1)
+            {
+                downThrust.transform.localScale = Vector3.Lerp(new Vector3(1, 0, 1), new Vector3(1, 1, 1), thrustLerpDistance);
+                thrustLerpDistance = thrustLerpDistance + thrustLerpSpeed * Time.deltaTime;
+            }
+
+        }
+        else
+        {
+            if (downThrust.transform.localScale.y > 0)
+            {
+                downThrust.transform.localScale = Vector3.Lerp(new Vector3(1, 0, 1), new Vector3(1, 1, 1), thrustLerpDistance);
+                thrustLerpDistance = thrustLerpDistance - thrustLerpSpeed * Time.deltaTime;
+            }
+        }
+        if (Input.GetAxis(horizontalID) !=0)
+        {
+            if (Input.GetAxis(horizontalID) > 0)
+            {
+                if (leftThrust.transform.localScale.y < 1)
+                {
+                    leftThrust.transform.localScale = Vector3.Lerp(new Vector3(1, 0, 1), new Vector3(1, 1, 1), leftthrustLerpDistance);
+                    leftthrustLerpDistance = leftthrustLerpDistance + thrustLerpSpeed * Time.deltaTime;
+                }
+            }
+
+            else if (Input.GetAxis(horizontalID) < 0)
+            {
+                if (rightThrust.transform.localScale.y < 1)
+                {
+                    rightThrust.transform.localScale = Vector3.Lerp(new Vector3(1, 0, 1), new Vector3(1, 1, 1), rightthrustLerpDistance);
+                    rightthrustLerpDistance = rightthrustLerpDistance + thrustLerpSpeed * Time.deltaTime;
+                }
+            }
+        }
+        else
+        {
+            if (leftThrust.transform.localScale.y > 0)
+            {
+                leftThrust.transform.localScale = Vector3.Lerp(new Vector3(1, 0, 1), new Vector3(1, 1, 1), leftthrustLerpDistance);
+                leftthrustLerpDistance = leftthrustLerpDistance - thrustLerpSpeed * Time.deltaTime;
+            }
+            if (rightThrust.transform.localScale.y > 0)
+            {
+                rightThrust.transform.localScale = Vector3.Lerp(new Vector3(1, 0, 1), new Vector3(1, 1, 1), rightthrustLerpDistance);
+                rightthrustLerpDistance = rightthrustLerpDistance - thrustLerpSpeed * Time.deltaTime;
+            }
+        }
+        #endregion  
+
     }
 
     public void OnCollisionEnter(Collision collision)
