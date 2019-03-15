d0b8b00a711c9ce7bcba271dc6cb58b71f1e24eb
Johan Andersson <johan.andersson@futuregames.nu>
2019-03-15 11:59:32 +0100

    Height stuffs

--- TetrisGodsGame/Assets/Scripts/Gameplay/GameManager.cs
+++ TetrisGodsGame/Assets/Scripts/Gameplay/GameManager.cs
@@ -19,34 +19,39 @@ public class GameManager : MonoBehaviour
             if (_instanceHelper == null)
             {
                 _instanceHelper = FindObjectOfType<GameManager>();
-                _instanceHelper.Setup();
+                if(_instanceHelper != null) _instanceHelper.Setup(); 
             }
+                
+                  
             if (_instanceHelper == null)
             {
                 _instanceHelper = new GameObject("GameManager").AddComponent<GameManager>();
                 _instanceHelper.Setup();
             }
-            
+    
             return _instanceHelper;
         }
     }
          
+    private BlockLists BlockList;
     private Dictionary<PlayerIndex, int> PlayerScore;
-    private bool IsPaused;
+    private bool IsPaused = true;
     private float CurrentTime;
     private GameplaySettings Settings;
-
+    private GameObject _floorTag;
     public static Action RoundOver;
-    
+
+
     private void Awake()
     {
-        if(Settings != null) return;
-
-        Setup();
+        IsPaused = true;
     }
+
 
     private void Setup()
     {
+        Debug.Log("Set up");
+        
         Settings = Instantiate(AssetDatabase.LoadAssetAtPath<GameplaySettings>("Assets/Scriptables/Settings/GameplaySettings.asset"));
         
         PlayerScore = new Dictionary<PlayerIndex, int>();
@@ -55,7 +60,21 @@ public class GameManager : MonoBehaviour
         {
             PlayerScore.Add((PlayerIndex)i, 0);
         }
-
+      
+        if (BlockList == null) 
+            BlockList = FindObjectOfType<BlockLists>();
+     
+        if (BlockList == null)  
+            BlockList = new GameObject("BlockList").AddComponent<BlockLists>();
+        
+        _floorTag = GameObject.FindWithTag("Floor");
+        if (_floorTag == null)
+        {
+            _floorTag = new GameObject();
+            _floorTag.tag = "Floor";
+        }
+        
+  
         IsPaused = false;
     }
 
@@ -79,15 +98,35 @@ public class GameManager : MonoBehaviour
         return (1 - _instance.Settings.BlockSpawnInterval.Evaluate(_instance.CurrentTime / _instance.Settings.RoundTime)) * _instance.Settings.MaxBlockSpawnInterval;
     }
 
+    public static float GetPlayerBlockHeight(PlayerIndex player)
+    {
+        float height = 0;
+        Debug.Log(_instance.BlockList);
+        BlockController[] blocks = _instance.BlockList.GetPlayerBlocks(player).ToArray();
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            float dist = _instance._floorTag.transform.position.y - blocks[i].transform.position.y;
+            dist = Mathf.Abs(dist);
+            if (dist > height)
+                height =  dist;
+        }
+       
+        return height;
+    }
+
     void Update()
     {
-        if(!IsPaused)
-            CurrentTime += Time.deltaTime;    
-
+        if (IsPaused) return;
+        
+        
+        CurrentTime += Time.deltaTime;    
+          
         if (CurrentTime > Settings.RoundTime)
         {
-            RoundOver.Invoke();
-        }
+            RoundOver?.Invoke();
             Debug.Log("Round over!");
+            IsPaused = true;
+        }
+           
     }
 }
