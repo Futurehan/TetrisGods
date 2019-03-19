6545f707c3896101027c96d45a7c5284962b7abf
karl <karlfyrkant@gmail.com>
2019-03-19 13:28:48 +0100

    Merge branch 'KarlBranch'

---
+++ TetrisGodsGame/Assets/Scripts/Blocks/GrabBlock1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabBlock1 : MonoBehaviour, IPowerUp
+{
+    public bool pulling = false;
+    public List<Rigidbody> rigidbodies = new List<Rigidbody>();
+
+
+    public void Shoot()
+    {
+        GetComponentInParent<Rigidbody>().isKinematic = true;
+        if (GetComponentInParent<BlockController>().Active == true)
+        {
+            GetComponentInParent<BlockController>().Spawn();
+        }
+        pulling = true;
+    }
+    public void AddBody(Rigidbody b)
+    {
+        rigidbodies.Add(b);
+    }
+    private void OnDestroy()
+    {
+        foreach (Rigidbody b in rigidbodies)
+        {
+            if (b!=null)
+            {
+                b.isKinematic = false;
+
+            }
+        }
+    }
+}
