af47e33a3a942ac88d8223f58660f461077f235f
karl <karlfyrkant@gmail.com>
2019-03-19 13:27:48 +0100

    Added base for magnetic block WIP

---
+++ TetrisGodsGame/Assets/Scripts/Blocks/GrabBlock.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabBlock : MonoBehaviour, IPowerUp
+{
+    public bool pulling = false;
+    public float atractForce = 30f;
+    private bool removeBody = true;
+    private Rigidbody body;
+    private List<Rigidbody> bodiesToPull =  new List<Rigidbody>();
+    private SphereCollider colider;
+    private BoxCollider box;
+    private Vector3 hoverLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponentInParent<Rigidbody>();
+        colider = GetComponent<SphereCollider>();
+        box = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(bodiesToPull.Count);
+        if (pulling)
+        {
+
+            if (bodiesToPull.Count > 0)
+            {
+
+
+                foreach (Rigidbody b in bodiesToPull)
+                {
+                    Vector3 forceDirection = (gameObject.transform.position - b.transform.position).normalized * atractForce;
+                    b.AddForce(forceDirection*Time.deltaTime);
+                    print("Pulling");
+                }
+            }
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody b = other.GetComponentInParent<Rigidbody>();
+
+        if (b != null)
+        {
+            AddBodyToList(b);
+            print("is not null");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Rigidbody b = other.GetComponentInParent<Rigidbody>();
+
+        if (b != null)
+        {
+            RemoveBodyFromList(b);
+            print("is not null");
+        }
+    }
+
+
+    public void AddBodyToList(Rigidbody rigidbody)
+    {
+        bodiesToPull.Add(rigidbody);
+    }
+
+
+
+    public void RemoveBodyFromList(Rigidbody rigidbody)
+    {
+        bodiesToPull.Remove(rigidbody);
+    }
+
+    public void Shoot()
+    {
+        body.constraints = RigidbodyConstraints.FreezeAll;
+        colider.enabled = true;
+        box.enabled = false;
+        pulling = true;
+        print("PULLING");
+    }
+}
---
+++ TetrisGodsGame/Assets/Scripts/Blocks/StuckBlock.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuckBlock : MonoBehaviour
+{
+    public GrabBlock1 master;
+    public BlockController blockMaster;
+    public GameObject Object;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (master.pulling == true)
+        {
+  
+            other.gameObject.GetComponentInParent<Rigidbody>().isKinematic = true;
+            master.AddBody(other.gameObject.GetComponentInParent<Rigidbody>());
+
+
+        }
+
+    
+      
+
+        }
+
+    }
+  
+
+
