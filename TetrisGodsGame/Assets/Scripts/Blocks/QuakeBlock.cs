3a0ed9f628e9fbb9cbdbe808616906a7e0b30aa7
karl <karlfyrkant@gmail.com>
2019-03-20 10:02:50 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ TetrisGodsGame/Assets/Scripts/Blocks/QuakeBlock.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuakeBlock : MonoBehaviour , IPowerUp
+{
+
+    GameObject targetPlatform;
+
+
+    public void Shoot()
+    {
+       GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("1");
+        //foreach (GameObject game in gameObjects)
+        //{
+
+        //}
+
+        if (gameObject.transform.position.x < 0)
+        {
+            foreach (GameObject g in gameObjects)
+            {
+                if (g.transform.position.x > 0)
+                {
+                    targetPlatform = g;
+                }
+            }
+        }
+        else
+        {
+            foreach (GameObject g in gameObjects)
+            {
+                if (g.transform.position.x < 0)
+                {
+                    targetPlatform = g;
+                }
+            }
+        }
+
+        targetPlatform.GetComponent<Animation>().Play();
+
+    }
+
+}
